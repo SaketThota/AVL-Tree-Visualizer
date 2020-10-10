056979c3a65f56231e193e392b1d87616cb7b3fe
SaketThota <saketthota98@gmail.com>
2020-10-10 10:50:14 +0530

    Reduced Cells gap

--- sketch.js
+++ sketch.js
@@ -1,9 +1,19 @@
+var w = window.innerWidth,
+    h = window.innerHeight,
+    tools = document.querySelector(".tools-wrapper").offsetWidth;
+
 function setup() { 
-    let canvas = createCanvas( window.innerWidth - document.querySelector(".tools-wrapper").offsetWidth, window.innerHeight);   
+    let canvas = createCanvas(w-tools , h);   
 
     canvas.parent('canvas');
     canvas.style('display', 'block');
     background(100);
+}
+
+window.onresize = function () {
+    w = window.innerWidth;
+    h = window.innerHeight;  
+    canvas.size(w-tools, h);
 }
 
 function draw() { 
